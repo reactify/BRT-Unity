5138d45b9630b2402303105bd8b74adf7e3b61e8
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2025-05-22 14:29:40 +0100

    Add directivity and nfcFilter entries to configuration

--- brt-unity-package/Runtime/Scripts/BRT/BRTConfiguration.cs
+++ brt-unity-package/Runtime/Scripts/BRT/BRTConfiguration.cs
@@ -8,8 +8,12 @@ namespace BRT
     {
         public List<HRTFResource> hrtfResources = new();
         public List<BRIRResource> brirResources = new();
+        public List<DirectivityResource> directivityResources = new();
+        public List<NFCFilterResource> nfcFilterResources = new();
 
         public const string HRTFResourceFolder = "Data/HRTF/";
         public const string BRIRResourceFolder = "Data/BRIR/";
+        public const string DirectivityResourceFolder = "Data/DirectivityTF/";
+        public const string NFCFilterResourceFolder = "Data/SOSFilters/";
     }
 }
